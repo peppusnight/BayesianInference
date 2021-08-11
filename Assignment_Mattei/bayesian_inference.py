d698879253d3eb5646466ae650a4be1d0edfce10
peppusnight <giuseppe.salerno89@gmail.com>
2021-08-11 12:16:35 +0200

    update: esercizi Mattei da 1 a 3 fatti

--- Assignment_Mattei/bayesian_inference.py
+++ Assignment_Mattei/bayesian_inference.py
@@ -1,12 +1,23 @@
 import pandas as pd
 import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
+from utils import *
+import copy
+from numpy.random import beta as beta
+from numpy.random import binomial
+
+def bern(p, size=None):
+    return binomial(n=1,p=p,size=size)
 
 print('Start!')
 '''
 1. Load the dataset in R (Filename: JOBSI HR.dta)
 '''
-data = pd.read_csv('JOBSII_HR.csv', index_col=None)
-data.pop('income')
+data_path = 'C:/Users/peppu/Documents/MyPythonProject/BayesianInference/Assignment_Mattei'
+filename = data_path + '/data/JOBSII_HR.csv'
+data_orig = pd.read_csv(filename)
+data_orig.pop('income')
 '''
  2. For each variable, calculate the mean for the whole sample and within
 each treatment group. For continuous covariates, also report the me-
@@ -14,6 +25,8 @@ dians, standard deviation and ranges within each treatment group.
 Record your results in a table. In a few sentences, comment on what
 you see and whether it is expected.
 '''
+print('Start 2!')
+data = data_orig.copy(deep=True)
 df = pd.DataFrame(data = {}, index = ['mean','median','std','range'])
 for d in data.columns:
 
@@ -30,7 +43,8 @@ for d in data.columns:
         df.loc['median', col_name] = data_sel[d].median()
         df.loc['std', col_name] = data_sel[d].std()
         df.loc['range', col_name] = data_sel[d].max()-data[d].min()
-df.to_csv('point_2.csv')
+df.to_csv(data_path + '/SavedOutput/point_2.csv')
+print('End 2!')
 
 '''
 3. Bayesian model-based analysis. For each scenario described below derive the posterior distributions of the finite sample average causal effect
@@ -38,22 +52,163 @@ and the super-population average causal effect. Plot the resulting posterior dis
 statistics of the resulting posterior distributions: mean, standard deviation, median, 2.5% and 97.5% percentiles.
 '''
 
-from utils import *
-print('Start!')
-data_path = r'C:\Users\peppu\Documents\MyPythonProject\BayesianInference\Assignment_Mattei\data/'
-filename = data_path + '/JOBSII_HR.csv'
-data = pd.read_csv(filename)
-
+######################################################################################################
+# 3 a i #
+######################################################################################################
+load_saved_model = True
+save_load_folder = 'SavedOutput'
+data = data_orig.copy(deep=True)
+print('Start 3 - a - i!')
 treat_var = 'Z'
 causal_var = 'depress6'
 par_prior = dict(nu_c=0, omega2_c=100^2, nu_t=0, omega2_t=100^2, a_c=2, b2_c=0.01, a_t=2, b2_t=0.01)
+if load_saved_model:
+    with open('{}/{}/model_3_a_i.json'.format(data_path,save_load_folder),'r') as fid:
+        chain_m_3_a_i = js.load(fid)
+    chain_m_3_a_i['Estimands'] = pd.DataFrame(chain_m_3_a_i['Estimands'])
+    chain_m_3_a_i['Theta'] = pd.DataFrame(chain_m_3_a_i['Theta'])
+else:
+    chain_m_3_a_i = model_3_a_i_ii(niter=50000, nburn=5000, thin=1,  par_prior=par_prior, Yobs=data[causal_var],W=data[treat_var],
+                                    seed=2021, theta_start=None, save_results=True)
+    with open('{}/{}/model_3_a_i.json'.format(data_path,save_load_folder),'w') as fid:
+        out_json = {}
+        out_json['Estimands'] = chain_m_3_a_i['Estimands'].to_dict()
+        out_json['Theta'] = chain_m_3_a_i['Theta'].to_dict()
+        js.dump(out_json, fid, indent=4)
+print(chain_m_3_a_i['Estimands'].describe())
+print(chain_m_3_a_i['Theta'].describe())
+chain_m_3_a_i['Estimands'].describe([0.025,0.5,0.975]).to_csv('{}/{}/point_3_a_i.csv'.format(data_path,save_load_folder))
 
-chain_mA = model_3_a_i(niter=5000, nburn=100, thin=1,  par_prior=par_prior, Yobs=data[causal_var], W=data[treat_var],
-                     seed=2021, theta_start=None, save_results=True)
-print(chain_mA['Estimands'].describe())
-print(chain_mA['Theta'].describe())
-print('End!')
+f, ax = plt.subplots(2,1,sharex=True,figsize=(9,6))
+merged_dist = np.hstack((chain_m_3_a_i['Estimands']['ate_fs'],chain_m_3_a_i['Estimands']['ate_sp'])); merged_dist.sort()
+bin_count,bins = np.histogram(merged_dist,40)
+ax[0].hist(chain_m_3_a_i['Estimands']['ate_fs'], bins=bins)
+ax[0].axvline(x=chain_m_3_a_i['Estimands']['ate_fs'].mean(), ymax=bin_count.max(), ymin=0, color='r')
+ax[0].set_ylabel('Bin count ATE_FS')
+ax[1].hist(chain_m_3_a_i['Estimands']['ate_sp'], bins=bins)
+ax[1].axvline(x=chain_m_3_a_i['Estimands']['ate_fs'].mean(), ymax=bin_count.max(), ymin=0, color='r')
+ax[1].set_ylabel('Bin count ATE_SP')
+ax[1].set_xlabel('Average Treatment Effect')
+for a in ax: a.grid();
+plt.show(block=False)
+f.savefig('{}/{}/point_3_a_i.png'.format(data_path,save_load_folder))
+print('End 3 - a - i!')
+######################################################################################################
+######################################################################################################
 
+######################################################################################################
+# 3 a ii #
+######################################################################################################
+load_saved_model = True
+save_load_folder = 'SavedOutput'
+print('Start 3 - a - ii!')
+df = data_orig.copy(deep=True)
+causal_var = 'depress6'
+df['y_log'] = np.log(data[causal_var])
+treat_var = 'Z'
+causal_var = 'y_log'
+par_prior = dict(nu_c=0, omega2_c=100^2, nu_t=0, omega2_t=100^2, a_c=2, b2_c=0.01, a_t=2, b2_t=0.01)
+if load_saved_model:
+    with open('{}/{}/model_3_a_ii.json'.format(data_path,save_load_folder),'r') as fid:
+        chain_m_3_a_ii = js.load(fid)
+    chain_m_3_a_ii['Estimands'] = pd.DataFrame(chain_m_3_a_ii['Estimands'])
+    chain_m_3_a_ii['Theta'] = pd.DataFrame(chain_m_3_a_ii['Theta'])
+else:
+    chain_m_3_a_ii = model_3_a_i_ii(niter=50000, nburn=5000, thin=1,  par_prior=par_prior, Yobs=df[causal_var],W=df[treat_var],
+                                 y_log=True,
+                                 seed=2021, theta_start=None, save_results=True)
+    with open('{}/{}/model_3_a_ii.json'.format(data_path,save_load_folder),'w') as fid:
+        out_json = {}
+        out_json['Estimands'] = chain_m_3_a_ii['Estimands'].to_dict()
+        out_json['Theta'] = chain_m_3_a_ii['Theta'].to_dict()
+        js.dump(out_json, fid, indent=4)
 
+print(chain_m_3_a_ii['Estimands'].describe())
+print(chain_m_3_a_ii['Theta'].describe())
+chain_m_3_a_ii['Estimands'].describe([0.025,0.5,0.975]).to_csv('{}/{}/point_3_a_ii.csv'.format(data_path,save_load_folder))
 
-print('End!')
+f, ax = plt.subplots(2,1,sharex=True,figsize=(9,6))
+merged_dist = np.hstack((chain_m_3_a_ii['Estimands']['ate_fs'],chain_m_3_a_ii['Estimands']['ate_sp'])); merged_dist.sort()
+bin_count,bins = np.histogram(merged_dist,40)
+ax[0].hist(chain_m_3_a_ii['Estimands']['ate_fs'], bins=bins)
+ax[0].axvline(x=chain_m_3_a_ii['Estimands']['ate_fs'].mean(), ymax=bin_count.max(), ymin=0, color='r')
+ax[0].set_ylabel('Bin count ATE_FS')
+ax[1].hist(chain_m_3_a_ii['Estimands']['ate_sp'], bins=bins)
+ax[1].axvline(x=chain_m_3_a_ii['Estimands']['ate_fs'].mean(), ymax=bin_count.max(), ymin=0, color='r')
+ax[1].set_ylabel('Bin count ATE_SP')
+ax[1].set_xlabel('Average Treatment Effect')
+for a in ax: a.grid();
+plt.show(block=False)
+f.savefig('{}/{}/point_3_a_ii.png'.format(data_path,save_load_folder))
+print('End 3 - a - ii!')
+######################################################################################################
+######################################################################################################
+
+######################################################################################################
+# 3 a ii #
+######################################################################################################
+print('Start 3 - b!')
+data = data_orig.copy(deep=True)
+treat_var = 'Z'
+causal_var = 'employ6'
+save_load_folder = 'SavedOutput'
+
+Nc = np.sum(data[treat_var].values == 0)
+Nt = np.sum(data[treat_var].values == 1)
+
+# Calculate the starting probability of the Bernoulli distribution
+pi_c = data.loc[data[treat_var].values==0,causal_var].sum()/np.sum(data[treat_var].values==0)
+pi_t = data.loc[data[treat_var].values==1,causal_var].sum()/np.sum(data[treat_var].values==1)
+
+# Prior distribution of pi_c and pi_t
+a_c, a_t = 1, 1
+b_c, b_t = 1, 1
+
+# Posterior distribution of the model parameters
+a_c_obs = a_c + data.loc[data[treat_var].values==0,causal_var].sum()
+b_c_obs = b_c + (1-data.loc[data[treat_var].values==0,causal_var]).sum()
+a_t_obs = a_t + data.loc[data[treat_var].values==1,causal_var].sum()
+b_t_obs = b_t + (1-data.loc[data[treat_var].values==1,causal_var]).sum()
+
+# Simulate
+np.random.seed(2021)
+n_iter = 50000
+ate_fs = []  # ATE of finite sample
+ate_sp = []  # ATE of super population
+for i in range(0,n_iter):
+
+    pi_c_sim = beta(a_c_obs, b_c_obs)  # Sample from psoterioe distribution of the controls
+    pi_t_sim = beta(a_t_obs, b_t_obs)  # Sample from psoterioe distribution of the treated
+
+    #Imputation of the missing potential outcomes
+    y1 , y0 = np.zeros(data.shape[0])*float('nan'), np.zeros(data.shape[0])*float('nan')
+
+    # Imputation of missing data for treated
+    y1[data[treat_var].values == 1] = data.loc[data[treat_var].values==1,causal_var]  # y1 treated are observed
+    y1[data[treat_var].values == 0] = bern(pi_t_sim, size = Nc)  # y1 non-treated are imputed
+
+    # Imputation of missing data for nontreated
+    y0[data[treat_var].values == 0] = data.loc[data[treat_var].values==0,causal_var]  # y0 non-treated are observed
+    y0[data[treat_var].values == 1] = bern(pi_c_sim, size = Nt)  # y0 treated are imputed
+
+    # Compute the ATEs
+    ate_fs.append(np.mean(y1)-np.mean(y0))  # average treatment effect of finite sample
+    ate_sp.append(pi_t_sim - pi_c_sim)  # average treatment effect of super population
+
+out_ate = pd.DataFrame(data={'ate_fs':ate_fs,'ate_sp':ate_sp})
+out_ate.describe([0.025,0.5,0.975]).to_csv('{}/{}/point_3_b.csv'.format(data_path,save_load_folder))
+print(out_ate.describe())
+
+f, ax = plt.subplots(2,1,sharex=True,figsize=(9,6))
+merged_dist = np.hstack((out_ate['ate_fs'],out_ate['ate_sp'])); merged_dist.sort()
+bin_count,bins = np.histogram(merged_dist,40)
+ax[0].hist(out_ate['ate_fs'], bins=bins)
+ax[0].axvline(x=out_ate['ate_fs'].mean(), ymax=bin_count.max(), ymin=0, color='r')
+ax[0].set_ylabel('Bin count ATE_FS')
+ax[1].hist(out_ate['ate_sp'], bins=bins)
+ax[1].axvline(x=out_ate['ate_fs'].mean(), ymax=bin_count.max(), ymin=0, color='r')
+ax[1].set_ylabel('Bin count ATE_SP')
+ax[1].set_xlabel('Average Treatment Effect')
+for a in ax: a.grid();
+f.savefig('{}/{}/point_3_b.png'.format(data_path,save_load_folder))
+print('End 3 - b!')
--- Assignment_Mattei/utils.py
+++ Assignment_Mattei/utils.py
@@ -5,24 +5,25 @@ import simplejson as js
 from numpy.random import normal as rnorm
 from numpy.random import chisquare as rchisq
 
-def model_3_a_i(niter,  par_prior, Yobs, W, nburn=None, thin=1, seed=None, theta_start=None, save_results=False):
+def model_3_a_i_ii(niter,  par_prior, Yobs, W, nburn=None, thin=1, seed=None, theta_start=None, y_log=False, save_results=False):
     '''
 
     :param niter:
-    :param nburn:
-    :param thin:
     :param par_prior:
     :param Yobs:
-    :param W: 
+    :param W:
+    :param nburn:
+    :param thin:
     :param seed:
     :param theta_start:
+    :param y_log:
     :param save_results:
     :return:
     '''
 
     # Set the seed for the simulation
     if seed is not None:
-        np.random.seed(0)
+        np.random.seed(seed)
 
     Nc = sum(1 - W) # Number of controls
     Nt = sum(W) # Number of treated
@@ -46,10 +47,10 @@ def model_3_a_i(niter,  par_prior, Yobs, W, nburn=None, thin=1, seed=None, theta
 
     # Parameter initialization
     if theta_start is None:
-        theta = {'mu_c' : np.mean(Yobs[W == 0]) + rnorm(0, 0.1, 1), # Y_c is normal with avg = mu_c
-                 'mu_t' :  np.mean(Yobs[W == 1]) + rnorm(0, 0.1, 1), # Y_t is normal with avg = mu_t
-                 'sigma2_c' : np.var(Yobs[W == 0]) + rnorm(0, 0.1, 1), # Y_c is normal with var = sigma2_c
-                 'sigma2_t' : np.var(Yobs[W == 1]) + rnorm(0, 0.1, 1) # Y_t is normal with var = sigma2_t
+        theta = {'mu_c' : np.mean(Yobs[W == 0]) + rnorm(0, 0.1), # Y_c is normal with avg = mu_c
+                 'mu_t' :  np.mean(Yobs[W == 1]) + rnorm(0, 0.1), # Y_t is normal with avg = mu_t
+                 'sigma2_c' : np.var(Yobs[W == 0]) + rnorm(0, 0.1), # Y_c is normal with var = sigma2_c
+                 'sigma2_t' : np.var(Yobs[W == 1]) + rnorm(0, 0.1) # Y_t is normal with var = sigma2_t
                  }
     else:
         theta = theta_start
@@ -67,22 +68,22 @@ def model_3_a_i(niter,  par_prior, Yobs, W, nburn=None, thin=1, seed=None, theta
         ##Update mu.c # Full conditional
         omega2_c_obs = 1 / (Nc / theta['sigma2_c'] + 1 / par_prior['omega2_c']) # Slide 34 - lesson short
         nu_c_obs = omega2_c_obs * ((yobs_c * Nc) / theta['sigma2_c'] +  par_prior['nu_c'] / par_prior['omega2_c']) # Slide 34 - lesson short
-        theta['mu_c'] = rnorm(nu_c_obs, np.sqrt(omega2_c_obs), 1)
+        theta['mu_c'] = rnorm(nu_c_obs, np.sqrt(omega2_c_obs))
 
         ##Update mu.t # Full conditional
         omega2_t_obs = 1 / (Nt / theta['sigma2_t'] + 1 / par_prior['omega2_t']) # Slide 34 - lesson short
         nu_t_obs = omega2_t_obs * ((yobs_t * Nt) / theta['sigma2_t'] +  par_prior['nu_t'] / par_prior['omega2_t']) # Slide 34 - lesson short
-        theta['mu_t'] = rnorm(nu_t_obs, np.sqrt(omega2_t_obs), 1)
+        theta['mu_t'] = rnorm(nu_t_obs, np.sqrt(omega2_t_obs))
 
         ##Update sigma2.c # Full conditional
         a_c_obs = Nc + par_prior['a_c'] # Slide 34 - lesson short
         b2_c_obs =  (par_prior['a_c'] * par_prior['b2_c'] + np.sum((Yobs[W == 0]-theta['mu_c']) ** 2)) / a_c_obs # Slide 34 - lesson short
-        theta['sigma2_c'] =  (a_c_obs * b2_c_obs) / rchisq(a_c_obs, 1)
+        theta['sigma2_c'] =  (a_c_obs * b2_c_obs) / rchisq(a_c_obs)
 
         ##Update sigma2.t
         a_t_obs = Nt + par_prior['a_t'] # Slide 34 - lesson short
         b2_t_obs =  (par_prior['a_t'] * par_prior['b2_t'] + np.sum((Yobs[W == 1]-theta['mu_t']) ** 2)) / a_t_obs # Slide 34 - lesson short
-        theta['sigma2_t'] =  (a_t_obs * b2_t_obs) / rchisq(a_t_obs, 1)
+        theta['sigma2_t'] =  (a_t_obs * b2_t_obs) / rchisq(a_t_obs)
 
         # rm(omega2.c.obs, nu.c.obs, omega2.t.obs, nu.t.obs, a.c.obs, b2.c.obs, a.t.obs, b2.t.obs)
 
@@ -93,7 +94,7 @@ def model_3_a_i(niter,  par_prior, Yobs, W, nburn=None, thin=1, seed=None, theta
             for k,v in theta.items():
                 Theta.loc[j, k] = v
 
-            ##FINITE SAMPLE ATE
+
             ##Imputate the missing potential outcomes using Ymis | Yobs, W, X, theta
             Y0, Y1 = np.zeros(Yobs.shape[0])*float('nan'), np.zeros(Yobs.shape[0])*float('nan')
 
@@ -103,10 +104,134 @@ def model_3_a_i(niter,  par_prior, Yobs, W, nburn=None, thin=1, seed=None, theta
             Y1[W == 0] = rnorm(theta['mu_t'], np.sqrt(theta['sigma2_t']), Nc)
             Y1[W == 1] = Yobs[W == 1]
 
-            Estimands.loc[j, "ate_fs"] = np.mean(Y1)-np.mean(Y0)
+            if y_log==False:
+                ##FINITE SAMPLE ATE
+                Estimands.loc[j, "ate_fs"] = np.mean(Y1)-np.mean(Y0)
 
-            ##SUPER-POPULATION ATE
-            Estimands.loc[j, "ate_sp"] = theta['mu_t']-theta['mu_c']
+                ##SUPER-POPULATION ATE
+                Estimands.loc[j, "ate_sp"] = theta['mu_t']-theta['mu_c']
+            else:
+                ##FINITE SAMPLE ATE
+                Estimands.loc[j, "ate_fs"] = np.mean(np.exp(Y1))-np.mean(np.exp(Y0))
+
+                ##SUPER-POPULATION ATE
+                Estimands.loc[j, "ate_sp"] = np.exp(theta['mu_t']+0.5*theta['sigma2_t']**2)-np.exp(theta['mu_c']+0.5*theta['sigma2_c']**2)
+
+    if save_results:
+        model = dict(Theta=Theta.to_dict(), Estimands=Estimands.to_dict())
+        with open('model_3_a_i.json','w') as fid:
+            js.dump(model, fid, indent=4)
+
+    return dict(Theta = Theta, Estimands = Estimands)
+
+def model_3_b(niter,  par_prior, Yobs, W, nburn=None, thin=1, seed=None, theta_start=None, y_log=False, save_results=False):
+    '''
+
+    :param niter:
+    :param par_prior:
+    :param Yobs:
+    :param W:
+    :param nburn:
+    :param thin:
+    :param seed:
+    :param theta_start:
+    :param y_log:
+    :param save_results:
+    :return:
+    '''
+
+    # Set the seed for the simulation
+    if seed is not None:
+        np.random.seed(seed)
+
+    Nc = sum(1 - W) # Number of controls
+    Nt = sum(W) # Number of treated
+    N = Nt + Nc # Total number of cases
+
+    yobs_c = np.mean(Yobs[W == 0]) # Average of the controls
+    yobs_t = np.mean(Yobs[W == 1]) # Average of the treated
+
+    ##Setup - MCMC
+    nburn = niter/2 if nburn is None else nburn # Number of burn in
+
+    draws = range((nburn + 1), niter+1, thin) # Number of draws
+
+    if (max(draws) < niter):
+        print("The number of iterations will be changed from {} to {}".format(niter, max(draws)+thin))
+        niter = max(draws)+thin
+        draws = range((nburn + 1), niter+1, thin)
+
+    ndraws = len(draws) # Number of iterations
+    j = -1  ##Counter j=1...ndraws
+
+    # Parameter initialization
+    if theta_start is None:
+        theta = {'pi_c' : len(Yobs[W == 0])/len(Yobs) + rnorm(0, 0.01), # Y_c is bernoulli with p = pi_c
+                 'pi_t' : len(Yobs[W == 1])/len(Yobs) + rnorm(0, 0.01), # Y_t is bernoulli with p = pi_t
+                 }
+    else:
+        theta = theta_start
+
+    # Store posterior distribution of Y
+    Theta = pd.DataFrame(data={'pi_c':[float('nan')]*ndraws,'pi_t':[float('nan')]*ndraws})
+    # Store ate_fs and ate-sp
+    Estimands = pd.DataFrame(data={'ate_fs':[float('nan')]*ndraws,'ate_sp':[float('nan')]*ndraws})
+
+    for ell in range(1,niter+1):
+
+        # Gibbs resampling applied
+
+        ##Update mu.c # Full conditional
+        omega2_c_obs = 1 / (Nc / theta['sigma2_c'] + 1 / par_prior['omega2_c']) # Slide 34 - lesson short
+        nu_c_obs = omega2_c_obs * ((yobs_c * Nc) / theta['sigma2_c'] +  par_prior['nu_c'] / par_prior['omega2_c']) # Slide 34 - lesson short
+        theta['mu_c'] = rnorm(nu_c_obs, np.sqrt(omega2_c_obs))
+
+        ##Update mu.t # Full conditional
+        omega2_t_obs = 1 / (Nt / theta['sigma2_t'] + 1 / par_prior['omega2_t']) # Slide 34 - lesson short
+        nu_t_obs = omega2_t_obs * ((yobs_t * Nt) / theta['sigma2_t'] +  par_prior['nu_t'] / par_prior['omega2_t']) # Slide 34 - lesson short
+        theta['mu_t'] = rnorm(nu_t_obs, np.sqrt(omega2_t_obs))
+
+        ##Update sigma2.c # Full conditional
+        a_c_obs = Nc + par_prior['a_c'] # Slide 34 - lesson short
+        b2_c_obs =  (par_prior['a_c'] * par_prior['b2_c'] + np.sum((Yobs[W == 0]-theta['mu_c']) ** 2)) / a_c_obs # Slide 34 - lesson short
+        theta['sigma2_c'] =  (a_c_obs * b2_c_obs) / rchisq(a_c_obs)
+
+        ##Update sigma2.t
+        a_t_obs = Nt + par_prior['a_t'] # Slide 34 - lesson short
+        b2_t_obs =  (par_prior['a_t'] * par_prior['b2_t'] + np.sum((Yobs[W == 1]-theta['mu_t']) ** 2)) / a_t_obs # Slide 34 - lesson short
+        theta['sigma2_t'] =  (a_t_obs * b2_t_obs) / rchisq(a_t_obs)
+
+        # rm(omega2.c.obs, nu.c.obs, omega2.t.obs, nu.t.obs, a.c.obs, b2.c.obs, a.t.obs, b2.t.obs)
+
+        # Save only specified iterations
+        if (ell in draws):
+            j = j+1
+
+            for k,v in theta.items():
+                Theta.loc[j, k] = v
+
+
+            ##Imputate the missing potential outcomes using Ymis | Yobs, W, X, theta
+            Y0, Y1 = np.zeros(Yobs.shape[0])*float('nan'), np.zeros(Yobs.shape[0])*float('nan')
+
+            Y0[W == 0] = Yobs[W == 0]
+            Y0[W == 1] = rnorm(theta['mu_c'], np.sqrt(theta['sigma2_c']), Nt)
+
+            Y1[W == 0] = rnorm(theta['mu_t'], np.sqrt(theta['sigma2_t']), Nc)
+            Y1[W == 1] = Yobs[W == 1]
+
+            if y_log==False:
+                ##FINITE SAMPLE ATE
+                Estimands.loc[j, "ate_fs"] = np.mean(Y1)-np.mean(Y0)
+
+                ##SUPER-POPULATION ATE
+                Estimands.loc[j, "ate_sp"] = theta['mu_t']-theta['mu_c']
+            else:
+                ##FINITE SAMPLE ATE
+                Estimands.loc[j, "ate_fs"] = np.mean(np.exp(Y1))-np.mean(np.exp(Y0))
+
+                ##SUPER-POPULATION ATE
+                Estimands.loc[j, "ate_sp"] = np.exp(theta['mu_t']+0.5*theta['sigma2_t']**2)-np.exp(theta['mu_c']+0.5*theta['sigma2_c']**2)
 
     if save_results:
         model = dict(Theta=Theta.to_dict(), Estimands=Estimands.to_dict())
